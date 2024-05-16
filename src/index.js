8833f85f149409e2b522e76484e4b5f5b35a84d6
HPerssn <henrik.perssn@gmail.com>
2024-05-16 20:45:04 +0200

    updated ui and slight temperature change based on time

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import { getLocation } from "./location.js";
 import { getWeather } from "./weather.js";
-
 import { animateTemperature } from "./counterAnimation.js";
+import { loader } from "./loader.js";
 
 document.getElementById("getWeather").addEventListener("click", function () {
   getLocation().then((position) => {
     getWeather(position.latitude, position.longitude)
       .then((temperatures) => {
-        animateTemperature(
-          temperatures.airTempCelcius,
-          temperatures.asphaltTemp
-        );
+        let asphaltTemp = temperatures.asphaltTemp;
+        if (position.hours < 7 || position.hours > 18) {
+          asphaltTemp -= 5; // minskar temp vid morgon och kväll
+        }
+        animateTemperature(temperatures.airTempCelcius, asphaltTemp);
       })
       .catch((error) => {
         console.error("Error getting weather data:", error);
       });
   });
 });
+
+window.onload = loader();
--- src/location.js
+++ src/location.js
@@ -3,9 +3,13 @@ export function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
+          const date = new Date(position.timestamp);
+          const hours = date.getHours();
           resolve({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
+            time: position.timestamp,
+            hours: hours,
           });
         },
         (error) => {
