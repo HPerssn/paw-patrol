60a6764a3e6f2dcf7118c6388f97a73ca39f14c2
HPerssn <henrik.perssn@gmail.com>
2024-05-20 11:28:00 +0200

    added correct paths

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 export function dropdown() {
   gsap.to("#weather", {
     height: "auto",
+    boxShadow: "0px 0px 20px rgba(0,0,0,0.2)",
     duration: 1,
     ease: "power2.inOut",
     onComplete: function () {
