71968f84e4413a54e2a7859f5e6f84acffd5134b
HPerssn <henrik.perssn@gmail.com>
2024-05-21 13:40:11 +0200

    added humidity and cloud cover to api fetch

--- src/components/weather.js
+++ src/components/weather.js
@@ -8,16 +8,17 @@ export function getWeather(lat, lon) {
       params: {
         latitude: lat,
         longitude: lon,
-        current: "temperature_2m",
+        current: ["temperature_2m", "cloud_cover", "relative_humidity_2m"],
         temperature_unit: "fahrenheit",
       },
     })
     .then((response) => {
+      let cloud_cover = response.data.current.cloud_cover;
       let airTemp = response.data.current.temperature_2m;
       let airTempCelcius = (airTemp - 32) * (5 / 9);
       let asphaltTemp = AsphaltTemperature(airTemp);
-      console.log(airTempCelcius, asphaltTemp);
-      return { asphaltTemp, airTempCelcius };
+      console.log(airTempCelcius, asphaltTemp, cloud_cover);
+      return { asphaltTemp, airTempCelcius, cloud_cover };
     })
     .catch((error) => {
       console.error("Error fetching weather data:", error);
