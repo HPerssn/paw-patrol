8112d3e45dd78ee390e7d48c9779b8e0b08a46d1
HPerssn <henrik.perssn@gmail.com>
2024-05-22 14:48:06 +0200

    small changes to color and gsap

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -4,6 +4,7 @@ export function dropdown() {
   gsap.to("#weather", {
     height: "auto",
     boxShadow: "0px 0px 20px rgba(0,0,0,0.2)",
+    border: "1px solid var(--main-text-color)",
     duration: 1,
     ease: "power2.inOut",
     onComplete: function () {
