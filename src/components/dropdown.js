4c0a82e9c77b8560cf4456dd08a2e305c62b6489
HPerssn <henrik.perssn@gmail.com>
2024-05-28 16:05:25 +0200

    changed colors again and removed image

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -4,7 +4,6 @@ export function dropdown() {
   gsap.to("#weather", {
     height: "auto",
     boxShadow: "0px 0px 20px rgba(0,0,0,0.2)",
-    border: "1px solid var(--main-text-color)",
     duration: 1,
     ease: "power2.inOut",
     onComplete: function () {
