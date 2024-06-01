edeaf71132be62150bfed6cd4383388b9f9df753
Henrik Persson <97438850+HPerssn@users.noreply.github.com>
2024-06-01 14:38:07 +0200

    Result info (#8)
    * small changes in dropdown and animations new animated button text
    
    * made humidity affect the air instead of pavement
    
    * added a +25 interval for air temp

--- src/components/resultText.js
+++ src/components/resultText.js
@@ -7,16 +7,20 @@ export function resultText(air, ground) {
   let airTempInterpretation;
 
   if (air > 30) {
-    airTempInterpretation = "It's incredibly hot today!";
+    airTempInterpretation = "It's incredibly hot today, be cautious!";
+  } else if (air > 25) {
+    airTempInterpretation = "It is starting to get hot, be cautious!";
   } else if (air > 22) {
     airTempInterpretation = "The air temperature is nice and warm.";
   } else {
     airTempInterpretation = "It's nice and chill!";
   }
-
   let text;
   if (ground > 40) {
     text = `Ouch! The pavement is burning hot. Not safe for your dog's paws. ${airTempInterpretation}`;
+  } else if (ground > 35) {
+    text = `The pavement is hot. Be cautious with your dog's paws. ${airTempInterpretation}`;
+
   } else if (ground > 30) {
     text = `The pavement is warm but still safe for your dog's stroll. ${airTempInterpretation}`;
   } else {
