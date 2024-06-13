0aaca51f511eded584405feefb5d5656357c3fba
HPerssn <henrik.perssn@gmail.com>
2024-06-13 19:29:08 +0200

    added webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,4 +13,9 @@ module.exports = {
     publicPath: "/",
     port: 5500,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
